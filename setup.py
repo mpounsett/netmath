f24ea81c42135556711f8f0dc633f8d9de8098a2
Matthew Pounsett <matt@conundrum.com>
2017-03-26 11:01:55 -0500

    renaming project

--- setup.py
+++ setup.py
@@ -1,33 +1,27 @@
-#!/usr/bin/env python
+"""
 
+"""
 from distutils.util import convert_path
-from pip.download import PipSession
-from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 main_ns = {}
-ver_path = convert_path('network_tools/version.py')
+ver_path = convert_path('netmath/version.py')
 with open(ver_path) as ver_file:
     exec(ver_file.read(), main_ns)
 
 setup(
-    name="network_tools",
+    name="netmath",
     version=main_ns['__VERSION__'],
     packages=find_packages(),
 
     author="Matthew Pounsett",
     author_email="matt@conundrum.com",
     description="Network Tools",
-    license="NONE",
-    keywords="game",
+    license="Apache Software License 2.0",
+    keywords="network library",
 
-    long_description="""
-    network_tools
-
-    """,
+    long_description=__doc__,
 
     install_requires=[
-        str(x.req) for x in parse_requirements('requirements.txt',
-                                               session=PipSession())
     ],
 )
